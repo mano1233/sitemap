b5bf5822a9e3916ba533bc7eb2024f5abd7c8d48
emanuel mena <emanuel.mena@appsflyer.com>
2023-11-27 15:18:55 +0200

    Init the project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module siteLink
+
+go 1.21.4
+
+require golang.org/x/net v0.18.0
--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"siteLink/link"
+)
+
+func main() {
+	url := flag.String("url", "", "the root url to begin the site mapping from (only urls of this domain will be searched)")
+	maxDepth := flag.Int("maxDepth", 4, "the amount of pages to search in from the root url")
+	flag.Parse()
+	response, err := http.Get(*url)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer response.Body.Close()
+	fmt.Printf("%#v", maxDepth)
+	var r io.Reader
+	link.Parse(r)
+}
